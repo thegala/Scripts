a2019a8b7dd58fa62d6aecebeb20bd1bbfdc26f3
Mirko Galic <mirkogalic@openaliasbox.org>
2015-03-02 12:41:23 +0100

    Move wget_download and fix it up

--- wget_manager/wget_download.sh
+++ wget_manager/wget_download.sh
@@ -7,17 +7,24 @@
 READ_FILE=$1
 DOWNLOAD_DIR=$2
 LOG_DIR=$3
-LOG_FILE=$LOG_DIR/
-OVER_FILE=$LOG_DIR/
+LOG_FILE=$LOG_DIR/$4
+OVER_FILE=$LOG_DIR/$5
 
 #While loop that go to download dir
 #remove line that contain link 1st line
 #and move link to log over file
-while read line 
-do
-    cd $DOWNLOAD_DIR
-    nohup wget $line > $LOG_FILE &
-    sed '1d'
-    echo $line >> $OVER_FILE 
-done < $READ_FILE
+stack(){
+    while read line 
+    do
+        cd $DOWNLOAD_DIR
+        nohup wget $line >> $LOG_FILE &
+        sed -i '1d' $READ_FILE
+        echo $line >> $OVER_FILE 
+    done < $READ_FILE
+}
 
+while true
+do
+    stack
+    sleep 5m
+done
